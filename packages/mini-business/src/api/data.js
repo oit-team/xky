a87e6132e397f710b02be9ccd9ae81ead34aec1d
李晓飞 <xiaofeiyu187@163.com>
2023-06-09 19:42:31 +0800

    feat: 修改首页

--- packages/mini-business/src/api/data.js
+++ packages/mini-business/src/api/data.js
@@ -1,5 +1,19 @@
 import { post } from './index'
 
+/**
+ * 服务预约
+ * @param {*} params
+ * @returns
+ */
 export function getAppointmentReport(params) {
   return post('/liveBroadcast/home/getAppointmentReport', params)
+}
+
+/**
+ * 抽奖互动
+ * @param {*} params
+ * @returns
+ */
+export function getLotteryInteraction(params) {
+  return post('/liveBroadcast/home/getLotteryInteraction', params)
 }
