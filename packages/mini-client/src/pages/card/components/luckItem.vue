3be88546f7baa6e3851031f74babba0e03e7de3f
邱旭 <uxuip@outlook.com>
2023-06-10 21:27:14 +0800

    fix: 修改距离显示

--- packages/mini-client/src/pages/card/components/luckItem.vue
+++ packages/mini-client/src/pages/card/components/luckItem.vue
@@ -17,10 +17,10 @@ export default {
     <view
       v-for="item in list"
       :key="item.shopId"
-      class="flex justify-between pr-3 py-3 bg-white rounded-xl mb-2 luck-item"
+      class="flex justify-between pr-3 py-3 bg-white rounded-xl mb-2 luck-item relative"
       @click="$emit('show', item)"
     >
-      <view class="flex width-3-5 items-center">
+      <view class="flex flex-1 items-center">
         <van-image
           width="70"
           height="70"
@@ -28,24 +28,26 @@ export default {
           class="mx-3 rounded-md bg-neutral-100 overflow-hidden"
         />
         <!-- :src="convertImageSize(item.headPortrait, 's')" -->
-        <view class="flex flex-col gap-2">
+        <view class="flex flex-col gap-2 flex-1">
           <view class="font-extrabold">
             {{ item.shopName }}
           </view>
           <view class="text-xs text-gray-500">
             地址：{{ item.address }}
           </view>
-          <view class="text-xs text-gray-500 ">
-            券数：<text class="text-orange-500">
-              {{ item.lotteryTicketNum }}
-            </text>
-          </view>
-          <view class="text-xs text-gray-500">
-            距离：{{ item.distance }}米
+          <view class="text-xs text-gray-500 flex justify-between">
+            <span>
+              券数：<text class="text-orange-500">
+                {{ item.lotteryTicketNum }}
+              </text>
+            </span>
+            <span>
+              距您 {{ item.distance }}km
+            </span>
           </view>
         </view>
       </view>
-      <view class="flex items-start text-sm">
+      <view class="flex items-start text-sm absolute right-2">
         <van-icon name="ellipsis" color="#6FA7FF" size="16" />
       </view>
     </view>
