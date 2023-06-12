0608e4077c91b3a6f8da8d440b8da8da8126be58
李晓飞 <xiaofeiyu187@163.com>
2023-06-12 18:37:58 +0800

    fix: 核销页面修改样式

--- packages/mini-business/src/pages/activity/confirm.vue
+++ packages/mini-business/src/pages/activity/confirm.vue
@@ -14,8 +14,17 @@ export default {
       opt: '',
       timer: null,
       isSuccess: false,
-      cardInfo: {},
+      cardInfo: {
+        // headPortrait: 'https://picsum.photos/200/300',
+        // wechatName: '张宇',
+        // vouchersName: '的说法',
+        // startTime: '2001/5/62',
+        // endTime: '2325/45/5',
+        // jackpotNote: '似懂非懂的点点滴反反复复凤飞飞反反复复方法滴哒哒哒哒哒哒',
+        // shopName: '的士速递的点点滴滴滴滴答答哒哒哒哒哒哒哒哒哒哒哒哒哒哒哒哒哒哒哒哒哒哒哒哒',
+      },
       nowTime: dayjs().format('YYYY/MM/DD HH:mm:ss'),
+      errMsg: '',
     }
   },
   computed: {
@@ -51,6 +60,7 @@ export default {
     async getData() {
       const res = await addPrizeExchangeRecord({ content: this.opt })
       if (res.head.status !== 0) {
+        this.errMsg = res.head.msg
         this.isSuccess = false
         clearInterval(this.timer)
       }
@@ -71,22 +81,27 @@ export default {
 </script>
 
 <template>
-  <container classes="h-full w-full flex flex-col items-center bg-gray-100">
-    <view v-if="logged" class="flex flex-col items-center px-2 mt-12">
-      <view class="mb-4 text-lg">
-        <view v-if="isSuccess" class="rounded-full p-2 bg-[#52c41a]">
-          <van-icon name="success" size="28" color="#fff" />
+  <container classes="h-full w-full flex flex-col items-center bg-gray-100 text-sm">
+    <view v-if="logged" class="flex flex-col flex-1 items-center w-full px-2 mt-12 box-border">
+      <view class="mb-4 w-full  text-center">
+        <view v-if="isSuccess" class="rounded-full w-80px h-80px flex justify-center items-center p-2 m-auto bg-[#52c41a]">
+          <van-icon name="success" size="40px" color="#fff" />
         </view>
-        <view v-else class="rounded-full p-2 bg-[#e84335]">
-          <van-icon name="cross" size="28" color="#fff" />
+        <view v-else class="rounded-full w-80px h-80px flex items-center justify-center bg-[#e84335] m-auto">
+          <van-icon name="cross" size="40px" color="#fff" />
         </view>
-        <view>{{ isSuccess ? '核销成功' : '核销失败' }}</view>
-        <view class="text-[#777] text-base">
+        <view class="text-lg my-2">
+          {{ isSuccess ? '核销成功' : '核销失败' }}
+        </view>
+        <view v-if="!isSuccess" class="text-[#777] mb-2">
+          {{ errMsg }}
+        </view>
+        <view class="text-[#777]">
           {{ nowTime }}
         </view>
       </view>
-      // 用户信息
-      <view v-if="isSuccess" class="w-full flex flex-col items-center p-2 mb-2 rounded-md box-border bg-white">
+      <!-- 用户信息 -->
+      <!-- <view v-if="isSuccess" class="w-full flex flex-col items-center p-2 mb-2 rounded-md box-border bg-white">
         <van-image
           width="60"
           height="60"
@@ -97,8 +112,8 @@ export default {
         <view class="text-center text-sm text-[#34495e]">
           {{ cardInfo.wechatName || cardInfo.weChatName }}
         </view>
-      </view>
-
+      </view> -->
+      <!-- 核销信息 -->
       <view v-if="isSuccess" class="w-full h-full rounded bg-white text-sm">
         <view class="flex justify-between items-center p-2 box-border">
           <view class="flex items-center">
@@ -106,33 +121,31 @@ export default {
           </view>
           <view class="box-border flex items-center text-xs" />
         </view>
-        <van-divider custom-style="{margin-top: 4px; margin-bottom: 4px;}" />
-        <view class="p-2 box-border flex">
+        <van-divider custom-style="margin: 0px;" />
+        <view class="p-2 box-border flex items-center">
           <van-image
-            width="60"
-            height="60"
+            width="80"
+            height="80"
             :src="cardInfo.resUrl"
             class="rounded-md"
           />
-          <view class="text-sm px-2 flex-1 flex flex-col leading-6 text-[#888]">
-            <view class="text-base font-600 text-black">
+          <view class="text-sm px-2 flex-1 flex flex-col leading-6 text-[#888] overflow-hidden">
+            <view class="text-base font-600 text-black w-full truncate">
               {{ cardInfo.vouchersName }}
             </view>
             <view>
               {{ cardInfo.startTime }}-{{ cardInfo.endTime }}
             </view>
-            <view>
+            <view class="w-full truncate">
               {{ cardInfo.jackpotNote || '暂无' }}
             </view>
-            <view>
+            <view class="w-full truncate">
               {{ cardInfo.shopName }}
             </view>
           </view>
         </view>
       </view>
-      <!-- <view class="w-full flex items-center bg-white rounded-md mb-2">
-      </view> -->
-      <view class="w-full  box-border">
+      <view class="w-full box-border  mt-2">
         <view class="w-full">
           <van-button
             block
